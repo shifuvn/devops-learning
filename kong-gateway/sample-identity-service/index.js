838ecf68d41a396de6f355ca6ccd59c4289a8bff
Hung Nguyen <hungntsol77@gmail.com>
2022-10-14 15:51:29 +0700

    kong jwt

---
+++ kong-gateway/sample-identity-service/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const https = require("https");
+const loggerRequest = require("logger-request-express");
+const os = require("os");
+
+const hostname = os.hostname();
+
+const app = express();
+
+//port
+const httpPort = 80;
+const httpsPort = 443;
+
+const server = http.createServer(app);
+
+app.all("*", loggerRequest);
+
+app.get("/secret", (req, res) => {
+	res.status(200).json({
+		data: {
+			secret: "abc123"
+		}
+	});
+});
+
+server.listen(httpPort, () => {
+	console.log(`Start on hostname ${hostname}`);
+	console.log(`Identity service is running on port ${httpPort}`);
+});

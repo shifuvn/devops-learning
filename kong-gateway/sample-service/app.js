38c568ffb1ec172703a5c9aa145b623e9ad8f7c1
Hung Nguyen <hungntsol77@gmail.com>
2022-10-14 10:00:58 +0700

    haproxy

--- kong-gateway/sample-service/app.js
+++ kong-gateway/sample-service/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const http = require("http");
 const https = require("https");
+const loggerRequest = require("logger-request-express");
+const os = require("os");
 
+const hostname = os.hostname();
 const app = express();
 const httpServer = http.createServer(app);
 
@@ -10,25 +13,45 @@ const httpServer = http.createServer(app);
  */
 const httpListeningPort = 80;
 
-const loggerClientIp = (req, route) => {
-	const ip = req.headers["x-forward-for"] || req.socket.remoteAddress;
-	console.log(`[${req.method}]  ${ip}  ${route}`);
-};
+app.all("*", loggerRequest);
+
+/**
+ * GET
+ */
 
 app.get("/", (req, res) => {
-	loggerClientIp(req, "/");
 	res.status(200).json({
 		data: { id: 1, status: "ok" }
 	});
 });
 
+app.get("/:name", (req, res) => {
+	res.status(200).json({
+		data: { id: 1, name: req.params.name, status: "ok" }
+	});
+});
+
+/**
+ * POST
+ */
+
 app.post("/:name", (req, res) => {
-	loggerClientIp(req, `/${req.params.name}`);
+	res.status(200).json({
+		status: `${req.params.name} ok`
+	});
+});
+
+/**
+ * PUT
+ */
+
+app.put("/:name", (req, res) => {
 	res.status(200).json({
 		status: `${req.params.name} ok`
 	});
 });
 
 httpServer.listen(httpListeningPort, () => {
+	console.log(`Start on ${hostname}`);
 	console.log(`Sample service is running on port ${httpListeningPort}`);
 });
